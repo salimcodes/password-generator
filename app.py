afe9dfb412e1e26aeca7112a65e99bf7d6cc760d
Salim Oyinlola <salimoyinlola@gmail.com>
2023-10-28 13:06:39 +0100

    My first commit!

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def generate_password():
             characters += string.punctuation
         
         password = generate_random_password(characters, password_length)
-        #password = ensure_first_and_last_letters(password, use_letters)
+        password = ensure_first_and_last_letters(password, use_letters)
         return render_template("index.html", password=password)
 
     return render_template("index.html")
@@ -32,7 +32,7 @@ def generate_password():
 def generate_random_password(characters, length):
     return "".join(random.choice(characters) for _ in range(length))
 
-"""
+
 def ensure_first_and_last_letters(password, use_letters):
     if use_letters and password and not password[0].isalpha():
         # Ensure first character is a letter
@@ -43,6 +43,6 @@ def ensure_first_and_last_letters(password, use_letters):
         password = password[:-1] + random.choice(string.ascii_letters)
 
     return password
-"""
+
 if __name__ == "__main__":
     app.run(debug=True)
